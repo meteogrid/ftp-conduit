e27ead2561d68119a23494fe921045ade158edb8
Alberto Valverde Gonzalez <alberto@meteogrid.com>
2015-03-18 17:48:35 +0100

    Revert "renamed close to closeConnection to avoid conflict with Socket's and using finally instead of catch to ensure cleanup"
    This reverts commit b7ae5d62f39a76f5c1fcde29a51fbaa69bb27469.

--- Network/FTP/Conduit.hs
+++ Network/FTP/Conduit.hs
@@ -19,11 +19,12 @@ import Data.Conduit.Network (sinkSocket, sourceSocket)
 import Control.Monad.Trans.Resource (MonadResource)
 import qualified Data.ByteString as BS
 import Data.ByteString.UTF8 hiding (foldl)
-import Network.Socket hiding (send, sendTo, recv, recvFrom, Closed)
+import Network.Socket hiding (close, send, sendTo, recv, recvFrom, Closed)
+import qualified Network.Socket as S (close)
 import Network.Socket.ByteString
 import Network.URI
 import Network.Utils
-import Control.Exception (Exception, throw, finally)
+import Control.Exception
 import Data.Word
 import System.ByteOrder
 import Data.Bits
@@ -76,42 +77,43 @@ writeLine s bs = do
   --putStrLn $ "Writing: " ++ (toString bs)
   sendAll s $ bs `BS.append` (fromString "\r\n") -- hardcode the newline for platform independence
 
-closeConnection :: (Socket, Socket) -> IO ()
-closeConnection (c, d) = finally (do
-    --putStrLn "Closing data connection"
-    close d
+close :: (Socket, Socket) -> IO ()
+close (c, d) = do
+  --putStrLn "Closing data connection"
+  S.close d
+  catch (do
     _ <- readExpected c 226
     writeLine c $ fromString "QUIT"
     _ <- readExpected c 221
     --putStrLn "Closing control connection"
-    close c
-    ) (close c)
+    S.close c
+    ) (\ e -> S.close c >> throw (e :: IOException))
 
 -- | Create a conduit source out of a 'URI'. Uses the @RETR@ command.
 createSource :: MonadResource m => URI -> Source m BS.ByteString
-createSource uri = bracketP setup closeConnection (sourceSocket . snd)
+createSource uri = bracketP setup close (sourceSocket . snd)
   where setup = do
           (c, d, path') <- common uri
-          finally (do
+          catch (do
             writeLine c $ fromString "TYPE I"
             _ <- readExpected c 200
             writeLine c $ fromString $ "RETR " ++ path'
             _ <- readExpected c 150
             return (c, d)
-            ) (close d >> close c)
+            ) (\ e -> S.close d >> S.close c >> throw (e :: IOException))
 
 -- | Create a conduit sink out of a 'URI'. Uses the @STOR@ command.
 createSink :: MonadResource m => URI -> Sink BS.ByteString m ()
-createSink uri = bracketP setup closeConnection (sinkSocket . snd)
+createSink uri = bracketP setup close (sinkSocket . snd)
   where setup = do
           (c, d, path') <- common uri
-          finally (do
+          catch (do
             writeLine c $ fromString "TYPE I"
             _ <- readExpected c 200
             writeLine c $ fromString $ "STOR " ++ path'
             _ <- readExpected c 150
             return (c, d)
-            ) (close d >> close c)
+            ) (\ e -> S.close d >> S.close c >> throw (e :: IOException))
 
 common :: URI -> IO (Socket, Socket, String)
 common (URI { uriScheme = scheme'
@@ -121,7 +123,7 @@ common (URI { uriScheme = scheme'
   if scheme' /= "ftp:" then throw $ IncorrectScheme scheme' else return ()
   --putStrLn "Opening control connection"
   c <- connectTCP host (PortNum (hton_16 port))
-  finally (do
+  catch (do
     _ <- readExpected c 220
     writeLine c $ fromString $ "USER " ++ user
     _ <- readExpected c 331
@@ -135,7 +137,7 @@ common (URI { uriScheme = scheme'
     --putStrLn "Opening data connection"
     d <- connectTCP (toString pasvhost) (PortNum (hton_16 pasvport))
     return (c, d, path')
-    ) (close c)
+    ) (\ e -> S.close c >> throw (e :: IOException))
   where (host, port, user, pass) = case authority' of
           Nothing -> undefined
           Just (URIAuth userInfo regName port') ->
